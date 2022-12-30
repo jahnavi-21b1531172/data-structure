800885846137fb4586a2800c81ac6c9e382cb0c8
jahnavi-21b1531172 <121659266+jahnavi-21b1531172@users.noreply.github.com>
2022-12-30 13:58:01 +0530

    Add files via upload

---
+++ ds practical.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int a[100],i,num,c=0,n;
+	printf("Enter size of array");
+	scanf("%d",&n);
+	printf("Enter array elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter no you want to search");
+	scanf("%d",&num);
+	for(i=0;i<n;i++)
+	{
+		if(num==a[i])
+		{
+			printf("%d is found at a[%d]\n",num,i);
+			c++;
+		}
+
+
